fc288c1cec5a60fcf0bc7741e28dd22f2ba73a7b
BernaRM111 <motoberna222@gmail.com>
2024-04-09 12:31:42 -0600

    add 1

---
+++ src/main/java/org/uv/DAPP02Practica02/HolaController.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/RestController.java to edit this template
+ */
+package org.uv.DAPP02Practica02;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Berna
+ */
+@RestController
+@RequestMapping("/holamundo")
+public class HolaController {
+    
+    @GetMapping
+    public String holamundo(){
+        return "Hola mundo";
+    }
+    
+    @GetMapping("/dos/")
+    public String holamundo2(){
+        return "Hola mundo dos...";
+    }
+    
+    @PostMapping("/dos/")
+    public String holamundo3(){
+        return "Hola mundo post...";
+    }
+}
